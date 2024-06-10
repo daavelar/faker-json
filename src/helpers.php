77515baf12e30702d848d3534178ffe4cf03b23d
Diego Avelar <diego.avelar@olist.com>
2024-06-10 10:05:44 -0300

    Ajuste nos helpers

--- src/helpers.php
+++ src/helpers.php
@@ -7,14 +7,22 @@ function tests_path(string $path)
     return __DIR__ . '/../tests/' . $path;
 }
 
-function base_path(string $path)
-{
-    return __DIR__ . '/../' . $path;
-}
-
 function dd($string)
 {
     var_dump($string);
 
     exit;
+}
+
+if (! function_exists('base_path')) {
+    /**
+     * Get the path to the base of the install.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function base_path($path = '')
+    {
+        return __DIR__ . '/../' . $path;
+    }
 }
